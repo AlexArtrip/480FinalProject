4ea4e6b2e506497abf888e7aec043d0547640128
Alex <aja10@rice.edu>
2021-04-20 00:52:05 -0500

    initialization of 480 final proj directory

---
+++ cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_42cb0.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_42cb0.dir/CMakeCUDACompilerABI.cu.obj"
+  "cmTC_42cb0.exe"
+  "cmTC_42cb0.exe.manifest"
+  "cmTC_42cb0.lib"
+  "cmTC_42cb0.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/cmTC_42cb0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_42cb0.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cmTC_42cb0.
+# This may be replaced when dependencies are built.
