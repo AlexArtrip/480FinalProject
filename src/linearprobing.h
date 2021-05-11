bb3d39fe7e64df05f053dde7d72abc447814f309
Alex <aja10@rice.edu>
2021-05-10 19:59:20 -0500

    fixed namespace issues

--- src/linearprobing.h
+++ src/linearprobing.h
@@ -3,15 +3,15 @@
 #include "hash_table.h"
 
 namespace LinearProbing {
-    KeyValue* create_hashtable(uint32_t size);
+    KeyValue* create_hashtable(std::uint32_t size);
 
-    void insert_hashtable(KeyValue* hashtable, uint32_t size, const KeyValue* kvs, uint32_t num_kvs);
+    void insert_hashtable(KeyValue* hashtable, std::uint32_t size, const KeyValue* kvs, std::uint32_t num_kvs);
 
-    void lookup_hashtable(KeyValue* hashtable, uint32_t size, KeyValue* kvs, uint32_t num_kvs);
+    void lookup_hashtable(KeyValue* hashtable, std::uint32_t size, KeyValue* kvs, std::uint32_t num_kvs);
 
-    void delete_hashtable(KeyValue* hashtable, uint32_t size, const KeyValue* kvs, uint32_t num_kvs);
+    void delete_hashtable(KeyValue* hashtable, std::uint32_t size, const KeyValue* kvs, std::uint32_t num_kvs);
 
-    std::vector<KeyValue> iterate_hashtable(KeyValue* hashtable, uint32_t size);
+    std::vector<KeyValue> iterate_hashtable(KeyValue* hashtable, std::uint32_t size);
 
     void destroy_hashtable(KeyValue* hashtable);
 
@@ -22,7 +22,7 @@ namespace LinearProbing {
             numKeyValues = hashTableCapacity / 2;
             table = create_hashtable(hashTableCapacity);
         }
-        HashTableLP(uint32_t size) {
+        HashTableLP(std::uint32_t size) {
             hashTableCapacity = size;
             numKeyValues = size / 2;
             table = create_hashtable(hashTableCapacity);
@@ -30,17 +30,17 @@ namespace LinearProbing {
         ~HashTableLP() {
             destroy_hashtable(table);
         }
-        virtual void insert_hashtable(const KeyValue* kvs, uint32_t num_kvs) {
-            insert_hashtable(table, kvs, num_kvs);
+        virtual void insert_hashtable(const KeyValue* kvs, std::uint32_t num_kvs) {
+            LinearProbing::insert_hashtable(table, hashTableCapacity, kvs, num_kvs);
         }
-        virtual void lookup_hashtable(KeyValue* kvs, uint32_t num_kvs) {
-            lookup_hashtable(table, kvs, num_kvs);
+        virtual void lookup_hashtable(KeyValue* kvs, std::uint32_t num_kvs) {
+            LinearProbing::lookup_hashtable(table, hashTableCapacity, kvs, num_kvs);
         }
-        virtual void delete_hashtable(const KeyValue* kvs, uint32_t num_kvs) {
-            delete_hashtable(table, kvs, num_kvs);
+        virtual void delete_hashtable(const KeyValue* kvs, std::uint32_t num_kvs) {
+            LinearProbing::delete_hashtable(table, hashTableCapacity, kvs, num_kvs);
         }
         virtual std::vector<KeyValue> iterate_hashtable() {
-            return iterate_hashtable(table);
+            return LinearProbing::iterate_hashtable(table, hashTableCapacity);
         }
     };
 }
